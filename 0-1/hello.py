60a874ac5811d8107c343fa40fa9fb4a5f97f281
James <Jamestylersalas8@gmail.com>
2025-08-10 15:37:33 +1000

    Hello.py

---
+++ 0-1/hello.py
@@ -0,0 +1,8 @@
+print('What is your name?')
+my_name = input('>')
+print('It\'s good to meet you, ' + my_name)
+print('The length of your name is:')
+print(len(my_name))
+print('What is your age?')
+my_age = input('>')
+print('You will be ' + str(int(my_age) + 1) + ' in a year.')
